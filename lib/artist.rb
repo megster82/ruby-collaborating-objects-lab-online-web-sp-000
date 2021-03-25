db39fe11a08ee29f9f0fb7ce3b5726295c5af06e
meaghan jennings <meaghanljennings@gmail.com>
2021-03-25 16:48:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,16 +8,16 @@ class Artist
     @@all << self 
   end
   
+  def self.all
+    @@all
+  end 
+  
   def add_song(song)
     @songs << song 
   end 
   
   def save
     @@all << self 
-  end 
-  
-  def self.all
-    @@all
   end 
   
   def self.find_by_name(artist_name)
