bd27c99e3fcee690468b3757ac54927ffab658e9
meaghan jennings <meaghanljennings@gmail.com>
2021-03-25 16:07:31 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,44 @@
+class Artist
+  attr_accessor :name, :artist_name, :songs
+
+    @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self
+  end
+  
+  def add_song(song)
+    @songs << song
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(artist_name)
+    found_artist = self.all.find do |artist|
+      artist.name == artist_name
+     end
+   end
+
+   def self.create_by_name(artist_name)
+     new_artist = Artist.new(artist_name)
+     new_artist.save
+     new_artist
+   end
+
+  def self.find_or_create_by_name(artist_name)
+    self.find_by_name(artist_name) || self.create_by_name(artist_name)
+  end
+
+  def print_songs
+    @songs.each {|song| puts song.name}
+  end
+  
+end
