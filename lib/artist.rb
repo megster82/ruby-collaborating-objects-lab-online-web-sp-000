e368d18bcee632e823eaa0615f0e075f875bccd8
meaghan jennings <meaghanljennings@gmail.com>
2021-03-25 16:31:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,13 +12,21 @@ class Artist
   end 
   
   def add_song(song)
-    song.artist = song 
+    @songs << song 
   end 
   
-  def songs
-    @songs
+  def self.find_by_name(name)
+    @@all.find{|x| x.name == name
+  end
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
   end
   
   def self.find_or_create_by_name(name)
-    
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
 end 
