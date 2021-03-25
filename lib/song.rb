861270f69ae970c217863cd52ed2c19fe53c5302
meaghan jennings <meaghanljennings@gmail.com>
2021-03-25 17:03:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,18 +11,18 @@ class Song
   def self.all
     @@all
   end
-  
+ 
   def self.new_by_filename(file_name)
     artist = file_name.split(" - ")[0]
     song_name = file_name.split(" - ")[1]
     song = self.new(song_name)
-    song.artist_name = artist 
+    song.artist_name = artist
     song
   end
   
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     self.artist.add_song(self)
-  end 
+  end
   
-end
+end 
